eeb42fe79c92637053cbd45b71c5a6b03fe5832d
blanquiaraujo <131007030+blanquiaraujo@users.noreply.github.com>
2023-12-17 22:56:08 +0100

    Add files via upload

--- fs/script.js
+++ fs/script.js
@@ -1,13 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     var elemento = document.documentElement;
+    var pantallaCompletaBtn = document.getElementById("pantallaCompletaBtn");
 
-    if (elemento.requestFullscreen) {
-        elemento.requestFullscreen();
-    } else if (elemento.mozRequestFullScreen) {
-        elemento.mozRequestFullScreen();
-    } else if (elemento.webkitRequestFullscreen) {
-        elemento.webkitRequestFullscreen();
-    } else if (elemento.msRequestFullscreen) {
-        elemento.msRequestFullscreen();
+    // Verifica si la API de pantalla completa está disponible
+    if (document.fullscreenEnabled ||
+        document.webkitFullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.msFullscreenEnabled) {
+
+        pantallaCompletaBtn.style.display = "block"; // Muestra el botón solo si la API de pantalla completa es compatible
+
+        // Función para activar el modo de pantalla completa
+        function activarPantallaCompleta() {
+            if (elemento.requestFullscreen) {
+                elemento.requestFullscreen();
+            } else if (elemento.mozRequestFullScreen) {
+                elemento.mozRequestFullScreen();
+            } else if (elemento.webkitRequestFullscreen) {
+                elemento.webkitRequestFullscreen();
+            } else if (elemento.msRequestFullscreen) {
+                elemento.msRequestFullscreen();
+            }
+        }
+
+        // Asigna la función al hacer clic en el botón
+        pantallaCompletaBtn.addEventListener("click", activarPantallaCompleta);
     }
 });
