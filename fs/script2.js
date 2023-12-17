bcfd46e7822767472b307242dc73f21c111887bb
blanquiaraujo <131007030+blanquiaraujo@users.noreply.github.com>
2023-12-17 23:43:21 +0100

    Add files via upload

--- fs/script2.js
+++ fs/script2.js
@@ -1,41 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     var elemento = document.documentElement;
+    var pantallaCompletaBtn = document.getElementById("pantallaCompletaBtn");
 
-    function activarPantallaCompleta() {
-        if (elemento.requestFullscreen) {
-            elemento.requestFullscreen();
-        } else if (elemento.mozRequestFullScreen) {
-            elemento.mozRequestFullScreen();
-        } else if (elemento.webkitRequestFullscreen) {
-            elemento.webkitRequestFullscreen();
-        } else if (elemento.msRequestFullscreen) {
-            elemento.msRequestFullscreen();
+    // Verifica si la API de pantalla completa está disponible
+    if (document.fullscreenEnabled ||
+        document.webkitFullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.msFullscreenEnabled) {
+
+        pantallaCompletaBtn.style.display = "block"; // Muestra el botón solo si la API de pantalla completa es compatible
+
+        // Función para activar el modo de pantalla completa
+        function activarPantallaCompleta() {
+            if (elemento.requestFullscreen) {
+                elemento.requestFullscreen();
+            } else if (elemento.mozRequestFullScreen) {
+                elemento.mozRequestFullScreen();
+            } else if (elemento.webkitRequestFullscreen) {
+                elemento.webkitRequestFullscreen();
+            } else if (elemento.msRequestFullscreen) {
+                elemento.msRequestFullscreen();
+            }
         }
-    }
 
-    function desactivarPantallaCompleta() {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
+        // Asigna la función al hacer clic en el botón
+        pantallaCompletaBtn.addEventListener("click", activarPantallaCompleta);
+
+        // Función para activar el modo de pantalla completa al girar el dispositivo a horizontal
+        function verificarOrientacion() {
+            if (window.innerWidth > window.innerHeight) {
+                activarPantallaCompleta();
+            }
         }
+
+        // Asigna la función al evento de cambio de orientación
+        window.addEventListener("orientationchange", verificarOrientacion);
     }
-
-    function manejarCambioOrientacion() {
-        if (window.orientation === 90 || window.orientation === -90) {
-            activarPantallaCompleta();
-        } else {
-            desactivarPantallaCompleta();
-        }
-    }
-
-    // Activa el modo de pantalla completa al cargar la página
-    activarPantallaCompleta();
-
-    // Escucha el evento de cambio de orientación
-    window.addEventListener("orientationchange", manejarCambioOrientacion);
 });
